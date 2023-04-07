067884db6573474e54d38ef774273660e687ce41
Agathe Herrou <herrou@grame.fr>
2023-04-07 17:29:54 +0200

    Tests

--- main.cpp
+++ main.cpp
@@ -109,10 +109,10 @@ int main()
     // A.testDiv();
     // A.testAtan2();
 
-    // A.testAnd();
+    A.testAnd();
     // A.testNot();
-    // A.testOr();
-    // A.testXor();
+    A.testOr();
+    A.testXor();
     
     /*{
         double u = 0.0;
